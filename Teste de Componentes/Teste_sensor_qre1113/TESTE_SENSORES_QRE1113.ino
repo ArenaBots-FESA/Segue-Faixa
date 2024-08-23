566d6dce39329f04cdde5a7bcf7e7da35aae5ccd
Alexandrecar <73361982+Alexandrecar@users.noreply.github.com>
2024-08-23 12:00:21 -0300

    Add files via upload

---
+++ Teste de Componentes/Teste_sensor_qre1113/TESTE_SENSORES_QRE1113.ino
@@ -0,0 +1,48 @@
+#define S1 A1
+#define S2 A2
+#define S3 A3
+
+bool Leitura1 = 0;
+bool Leitura2 = 0;
+bool Leitura3 = 0;
+
+void setup() {
+  Serial.begin(9600); // Setup the Serial to a speed of 9600bps
+  
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);   
+  pinMode(S3, INPUT);
+}
+
+void loop() {
+  // Print the sensors values
+
+  if(analogRead(S1) <= 500){
+    Leitura1 = 1;    
+  }
+  else{
+    Leitura1 = 0;
+    }
+
+  if(analogRead(S2) <= 500){
+    Leitura2 = 1;    
+  }
+  else{
+    Leitura2 = 0;
+    }
+
+  if(analogRead(S3) <= 500){
+    Leitura3 = 1;    
+  }
+  else{
+    Leitura3 = 0;
+    }
+
+  Serial.println(Leitura1);
+  Serial.println("        ");
+  Serial.println(Leitura2);
+  Serial.println("        ");
+  Serial.println(Leitura3);
+  Serial.println("---------------------------------");
+  Serial.println("\n");
+}
